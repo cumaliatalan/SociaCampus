862e4e988cb0f07784ce02d92032c5adee866f46
Halit Seyfullah Baskurt <hbaskurt@invio.com>
2023-06-17 23:10:41 +0300

    event detaya da favori özelliği eklendi

--- SociaCampus/EventDetail/EventDetailViewController.swift
+++ SociaCampus/EventDetail/EventDetailViewController.swift
@@ -16,7 +16,8 @@ class EventDetailViewController: UIViewController {
     @IBOutlet weak var eventLocation: UILabel!
     @IBOutlet weak var eventDate: UILabel!
     @IBOutlet weak var eventDesc: UITextView!
-    
+    private var isFavorite: Bool?
+    @IBOutlet weak var favoriteButton: UIButton!
     private (set) var detailData : Event?
     
     override func viewDidLoad() {
@@ -28,15 +29,33 @@ class EventDetailViewController: UIViewController {
         eventDesc.text = detailData.eventDesc
         eventDate.text = detailData.eventDate
         eventImageView.sd_setImage(with: URL(string: detailData.eventPhotoURL))
-        
+        self.isFavorite = LFManager.shared.checkIsFavorite(favoriteID: detailData.eventId)
+        updateFavoriteIcon(isFavorite: self.isFavorite)
     }
     
-    func setupData(detailData: Event) {
-        self.detailData = detailData
-    }
+    func setupData(detailData: Event) { self.detailData = detailData }
     
     @IBAction func backButtonAction(_ sender: Any) {
         performSegue(withIdentifier: "toTabbarfromDetail", sender: nil)
     }
     
+    @IBAction func favoriteButtonAction(_ sender: Any) {
+        guard let detailData, let isFavorite = self.isFavorite  else { return }
+        if isFavorite {
+            LFManager.shared.removeFromFavorite(favoriteID: detailData.eventId)
+        } else {
+            LFManager.shared.addToFavorite(for: detailData.eventId)
+        }
+        self.isFavorite?.toggle()
+        updateFavoriteIcon(isFavorite: self.isFavorite)
+    }
+    
+    private func updateFavoriteIcon(isFavorite: Bool?) {
+        guard let isFavorite = self.isFavorite else { return }
+        DispatchQueue.main.async {
+            self.favoriteButton.imageView?.image = isFavorite ?
+            UIImage(systemName: "heart.fill") : UIImage(systemName: "heart")
+        }
+        
+    }
 }
